5005729bf576a49415b2efeedd55223828bef7a4
Eric W. Greene <eric@ericwgreene.com>
2017-12-15 09:29:01 -0500

    added code demos

--- src/app/color-tool/services/colors.service.ts
+++ src/app/color-tool/services/colors.service.ts
@@ -6,16 +6,37 @@ import { Color } from '../models/color';
 @Injectable()
 export class ColorsService {
 
+  private baseUrl = 'http://localhost:3050/colors';
+
   constructor(private httpClient: HttpClient) { }
 
+  private getCollectionURL() {
+    return this.baseUrl;
+  }
+
+  private getElementURL(id: number) {
+    return this.baseUrl + '/' + encodeURIComponent(id.toString());
+  }
+
   public all() {
-    return this.httpClient.get<Color[]>('http://localhost:3050/colors').toPromise();
+    return this.httpClient.get<Color[]>(this.getCollectionURL()).toPromise();
+  }
+
+  public one(id: number) {
+    return this.httpClient.get<Color>(this.getElementURL(id)).toPromise();
   }
 
   public delete(colorId: number) {
     return this.httpClient
-      .delete<void>('http://localhost:3050/colors/' + colorId.toString())
-      .toPromise();
+      .delete<void>(this.getElementURL(colorId)).toPromise();
+  }
+
+  public insert(color: Color) {
+    return this.httpClient.post<Color>(this.getCollectionURL(), color).toPromise();
+  }
+
+  public replace(color: Color) {
+    return this.httpClient.put<Color>(this.getElementURL(color.id), color).toPromise();
   }
 
 }
